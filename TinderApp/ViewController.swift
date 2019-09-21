2ca6e057b3c3ebeff749e2f44fea087a59c69037
u-sk <u.eggplant@gmail.com>
2019-09-21 09:29:05 +0900

    Initial Commit

---
+++ TinderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderApp
+//
+//  Created by 板垣有祐 on 2019/09/21.
+//  Copyright © 2019 Yusuke Itagaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
